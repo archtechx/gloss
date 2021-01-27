dd346dc446cc0a1caff9de8202a8642c2964e123
Samuel Štancl <samuel.stancl@gmail.com>
2021-01-27 12:28:31 +0100

    Add conditions to facade docblocks

--- src/Gloss.php
+++ src/Gloss.php
@@ -9,9 +9,9 @@ use Illuminate\Support\Facades\Facade;
 /**
  * 🔍 Gloss ✨ — Brilliant localization for Laravel.
  *
- * @method static void key(string $shortKey, string $newKey) Set a key override.
- * @method static void value(string $shortKey, string $value) Set a value override.
- * @method static void values(string $shortKey, string $value) Set multiple value overrides.
+ * @method static void key(string $shortKey, string $newKey, array|null|callable $condition) Set a key override.
+ * @method static void value(string $shortKey, string $value, array|null|callable $condition) Set a value override.
+ * @method static void values(string $shortKey, array|null|callable $condition) Set multiple value overrides.
  * @method static ?string get($key, $replace = [], $locale = null) Get a translation string.
  * @method static ?string choice($key, $replace = [], $locale = null) Get a translation according to an integer value.
  * @method static void extend(string $shortKey, callable(string, callable): string $value) Extend a translation string.
